e19f6d462845bcd6dcc48c2dc976df437d390d80
Ken Riippa <47590652+kenrii@users.noreply.github.com>
2023-11-20 21:34:38 +0200

    Update getRecipes.js

--- functions/getRecipes.js
+++ functions/getRecipes.js
@@ -38,6 +38,9 @@ exports.handler = async (event, context) => {
     };
    
     return {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
       statusCode: 200,
       body: JSON.stringify(data),
     };
