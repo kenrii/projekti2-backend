56f8064d9964fc15e67f6d8da96825bebdebf45c
Ken Riippa <47590652+kenrii@users.noreply.github.com>
2023-11-20 21:05:09 +0200

    Add files via upload

---
+++ functions/getRecipes.js
@@ -0,0 +1,45 @@
+exports.handler = async (event, context) => {
+    const data = {
+      featuredRecipe: {
+        name: "Featured Recipe",
+        ingredients: ["Ingredient 1", "Ingredient 2"],
+        steps: ["Step 1", "Step 2"]
+      },
+      category1: {
+        name: "Category 1", 
+        recipes: [
+          {
+            name: "Recipe 1", 
+            ingredients: ["Ingredient 1", "Ingredient 2"],
+            steps: ["Step 1", "Step 2"]
+          },
+          {
+            name: "Recipe 2",
+            ingredients: ["Ingredient 1", "Ingredient 2"],
+            steps: ["Step 1", "Step 2"]
+          }
+        ]
+      },
+      category2: {
+        name: "Category 2", 
+        recipes: [
+          {
+            name: "Recipe 3",
+            ingredients: ["Ingredient 1", "Ingredient 2"],
+            steps: ["Step 1", "Step 2"] 
+          },
+          {
+            name: "Recipe 4",
+            ingredients: ["Ingredient 1", "Ingredient 2"],
+            steps: ["Step 1", "Step 2"]
+          }
+        ]
+      }
+    };
+   
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+   };
+   
